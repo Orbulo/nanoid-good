8baf39408aecfeb33871502cd9f5e8247b5ed8ef
Leon Si <leonzalion@gmail.com>
2021-02-25 16:06:32 -0500

    moved nanoid good into its own repository

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { customAlphabet } from 'nanoid';
+import naughtyWords from 'naughty-words/en.json';
+
+function hasNaughtyWord(id) {
+	const idWithNumbersReplaced = id
+		.replace('1', 'i')
+		.replace('3', 'e')
+		.replace('5', 's')
+		.replace('8', 'b')
+		.replace('0', 'o');
+
+	for (const naughtyWord of naughtyWords) {
+		const word = naughtyWord.replace(/ /, '').replace('l', 'i');
+		if (new RegExp(word, 'gi').test(idWithNumbersReplaced)) return true;
+	}
+	return false;
+}
+
+export function nanoid(size = 30) {
+	const gen = customAlphabet('6789BCDFGHJKLMNPQRTWbcdfghjkmnpqrtwz', size);
+
+	let id = gen();
+	while (hasNaughtyWord(id)) {
+		id = gen();
+	}
+
+	return id;
+}
